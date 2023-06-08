42b263e04cde6896f695f605d627aa9aff97b3a2
Chalas99 <85922129+Chalas99@users.noreply.github.com>
2023-06-08 23:12:12 +0530

    added the customer model

---
+++ src/models/customer.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  contactNum: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+const Customer = mongoose.model("Customers", customerSchema);
+
+module.exports = Customer;
